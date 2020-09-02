e46c6e3413e76f060f6a49b8e7fccff18c4ec9d7
Sekori1 <romaric.gauzi1@gmail.com>
2020-09-02 10:54:00 +0200

    env

--- src-functions/login.js
+++ src-functions/login.js
@@ -1,9 +1,14 @@
 export function handler(event, context, callback) {
-  var env = process.env;
+  var {
+    SPOTIFY_CLIENT_ID, 
+    SPOTIFY_CLIENT_SECRET,
+    SPOTIFY_REDIRECT_URI
+  } = process.env;
 
+  
   console.log(event)
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: "Hello, World!", env: env})
+    body: JSON.stringify({msg: "Hello, World!", SPOTIFY_CLIENT_ID})
   })
 }
