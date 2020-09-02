80fb018d56a50e43c2021b8002d4837755405e18
Sekori1 <romaric.gauzi1@gmail.com>
2020-09-02 10:48:47 +0200

    test env

--- src-functions/login.js
+++ src-functions/login.js
@@ -1,14 +1,4 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-
 export function handler(event, context, callback) {
-
-
-  var spotifyApi = new SpotifyWebApi({
-    clientId: 'fcecfc72172e4cd267473117a17cbd4d',
-    clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
-    redirectUri: 'http://www.example.com/callback'
-  });
-
   var env = process.env;
 
   console.log(event)
