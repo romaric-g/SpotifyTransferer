765b25dde5bcf991a8912b3fec0133f70e1b8258
Sekori1 <romaric.gauzi1@gmail.com>
2020-09-04 00:56:08 +0200

    change

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,13 +9,41 @@
       </div>
     </div>
     <div class="content px-2">
-      <template v-if="tracks.length > 0">
-        <div class="bg-white shadow-md rounded px-8 py-6 ">
-            <div v-for="(track, index) in tracks" :key="index" class="flex items-center my-1">
-              <t-checkbox class="mx-4" v-model="track.checked" /><p class="mx-4 flex-1">{{track.name}}</p>
-            </div>
+      <div class="bg-white shadow-md rounded px-8 py-6 ">
+        <div id="menu">
+          <span class="mr-4">
+            <t-checkbox :checked="allSelected" @change="setSelectAll" /><label class="mx-4 flex-1">Tout sélectionner</label>
+          </span>
+          <span class="mr-4">
+            <t-checkbox :checked="allTracksSelected" @change="select => setSelectAllArray(tracks,select)" /><label class="mx-4 flex-1">Titres</label>
+          </span>
+          <span class="mr-4">
+            <t-checkbox :checked="allFollowingSelected" @change="select => setSelectAllArray(following,select)" /><label class="mx-4 flex-1">Artistes suivis</label>
+          </span>
+          <span class="mr-4">
+            <t-checkbox :checked="allAlbumsSelected" @change="select => setSelectAllArray(albums,select)" /><label class="mx-4 flex-1">Albums</label>
+          </span>
         </div>
-      </template>
+        <hr class="border-solid">
+        <div id="tracks">
+          <div v-for="(track, index) in tracks" :key="index" class="flex items-center my-1">
+            <t-checkbox class="mr-4" v-model="track.checked" /><p class="mx-4 flex-1">{{track.name}}</p>
+          </div>
+        </div>
+        <hr class="border-solid">
+        <div id="following">
+          <div v-for="(follow, index) in following" :key="index" class="flex items-center my-1">
+            <t-checkbox class="mr-4" v-model="follow.checked" /><p class="mx-4 flex-1">{{follow.name}}</p>
+          </div>
+        </div>
+        <hr class="border-solid">
+        <div id="albums">
+          <div v-for="(album, index) in albums" :key="index" class="flex items-center my-1">
+            <t-checkbox class="mr-4" v-model="album.checked" /><p class="mx-4 flex-1">{{album.name}}</p>
+          </div>
+        </div>
+        <hr class="border-solid">
+      </div>
     </div>
   </div>
 </template>
@@ -30,27 +58,46 @@ export default {
   components: {AuthSpotify},
   data: function() {
     return {
-      tracks: [],
-      following: []
+      tracks: [],//[{name: "Track 1", checked: true},{name: "Track 2", checked: true},{name: "Track 3", checked: true}],
+      following: [],//[{name: "following 1", checked: true},{name: "following 2", checked: true},{name: "following 3", checked: true}],
+      albums: [],//[{name: "Album 1", checked: true},{name: "Album 2", checked: true},{name: "Album 3", checked: true}],
     }
   },
   mounted () {
     console.log(this)
     if(this.sourceToken) {
       this.pulltracks()
+      this.pullfollowing()
     }
     
   },
   methods: {
+    setSelectAll: function(select) {
+      console.log("CALL ALL")
+        console.log(this.allSelected + " 0 " + select)
+        if(this.allSelected || select) {
+          console.log("CALL ALL ok")
+          this.setSelectAllArray(this.tracks, select)
+          this.setSelectAllArray(this.following, select)
+          this.setSelectAllArray(this.albums, select)
+        }
+
+    },
+    setSelectAllArray: function(array, select) {
+      console.log("CALL")
+        array.forEach(element => {
+          element.checked = select
+        });
+
+    },
+    allIsSelected: function(array) {
+      return !array.some(e => e.checked === false)
+    },
     pulltracks: function(url = 'https://api.spotify.com/v1/me/tracks?limit=50') {
       return axios({
         method: 'get',
         url,
-        headers: {
-          'Authorization': `Bearer ${this.sourceToken}`,
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        }
+        headers: getHeader(this)
       }).then(response => {
         var body = response.data
         for (let i of body.items) {
@@ -60,9 +107,57 @@ export default {
         if (body.next)
           this.pulltracks(body.next)
       })
-    }
+    },
+    pullfollowing: function(url = 'https://api.spotify.com/v1/me/following?type=artist&limit=50') {
+      return axios({
+        method: 'get',
+        url,
+        headers: getHeader(this)
+      }).then(response => {
+        var body = response.data
+        for (let i of body.artists.items)
+          i.checked = true
+		      this.following.push(i)
+		    if (body.artists.next)
+          this.pullfollowing(body.artists.next)
+      })
+    },
+    pullalbums: function(url = 'https://api.spotify.com/v1/me/albums?limit=50') {
+      return axios({
+        method: 'get',
+        url,
+        headers: getHeader(this)
+      }).then(response => {
+        var body = response.data
+        for (let i of body.items)
+          i.album.checked = true
+          this.albums.push(i.album)
+        if (body.next)
+          this.pullalbums(body.next)
+      })
+    },
   },
-  computed: mapState(['sourceToken','targetToken']),
+  computed: {
+    allSelected: function() {
+      return (
+        this.allTracksSelected &&
+        this.allFollowingSelected &&
+        this.allAlbumsSelected
+      )
+    },
+    allTracksSelected: function() { return this.allIsSelected(this.tracks) },
+    allFollowingSelected: function() { return this.allIsSelected(this.following) },
+    allAlbumsSelected: function() { return this.allIsSelected(this.albums) },
+    ...mapState(['sourceToken','targetToken'])
+  },
+}
+
+function getHeader(instance) {
+  return  {
+            'Authorization': `Bearer ${instance.sourceToken}`,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          }
 }
 
 function pullfollowing(store, url = "https://api.spotify.com/v1/me/following?type=artist&limit=50") {
