cfc5fb1a8166d8302ea71cb3d7e547a9926175fe
Sekori1 <romaric.gauzi1@gmail.com>
2020-09-03 12:14:47 +0200

    tracks

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,59 @@
 <template>
-  <div class="flex justify-between">
-    <div class="w-2/5 px-2">
-      <AuthSpotify type="source" :token="sourceToken"/>
+  <div>
+    <div class="flex justify-between">
+      <div class="w-2/5 px-2">
+        <AuthSpotify type="source" :token="sourceToken"/>
+      </div>
+      <div class="w-2/5 px-2">
+        <AuthSpotify type="target" :token="targetToken"/>
+      </div>
     </div>
-    <div class="w-2/5 px-2">
-      <AuthSpotify type="target" :token="targetToken"/>
+    <div class="border-t-4 bg-white shadow-md rounded">
+      <div v-for="(track, index) in tracks" :key="index">
+        <p>{{track.name}}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import AuthSpotify from '@/components/AuthSpotify.vue'
+import axios from 'axios'
 import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
   components: {AuthSpotify},
-  created: function() {
-    console.log(this)
+  data: function() {
+    return {
+      tracks: []
+    }
+  },
+  mounted () {
+    pulltracks()
   },
   computed: mapState(['sourceToken','targetToken']),
 }
+
+function pulltracks() {
+    axios({
+      method: 'get',
+      url: 'https://api.coindesk.com/v1/bpi/currentprice.json',
+      headers: {
+        'Authorization': `Bearer ${$store.state.sourceToken}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      body = JSON.parse(body)
+      console.log("Pull tracks", body.offset + "/" + body.total)
+      for (let i of body.items)
+        tracks.push(i.track.id)
+      if (body.next)
+        pulltracks(body.next)
+    })
+}
+
 </script>
 
 <style scoped>
